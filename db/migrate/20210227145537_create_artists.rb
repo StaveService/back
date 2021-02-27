faa48263a5d6005cdce4c76a0f17f05eb6b82a43
izszzz <izszzz@.yahoo.co.jp>
2021-02-27 23:56:07 +0900

    add artist

---
+++ db/migrate/20210227145537_create_artists.rb
@@ -0,0 +1,15 @@
+class CreateArtists < ActiveRecord::Migration[5.2]
+  def change
+    create_table :artists do |t|
+      t.references :user, foreign_key: true
+      t.string :name
+      t.string :twitter
+      t.string :instagram
+      t.string :spotify
+      t.string :itunes
+      t.string :youtube
+
+      t.timestamps
+    end
+  end
+end
