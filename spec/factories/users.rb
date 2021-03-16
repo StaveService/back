160e844ea2459e3afd2b32d812bb54001662a3f2
izszzz <izszzz@.yahoo.co.jp>
2021-03-17 08:25:42 +0900

    update user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Name.unique.name }
     email { Faker::Internet.email }
-    familyname { Faker::Name.name }
-    givenname { Faker::Name.name }
+    familyname { Faker::Name.first_name }
+    givenname { Faker::Name.last_name }
     password { 'password' }
     password_confirmation { 'password' }
   end
