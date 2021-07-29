38c1441c6bcf24387414597a0484f5857fd2cccd
izszzz <izszzz@yahoo.co.jp>
2021-07-29 18:51:18 +0900

    update notification

--- app/graphql/types/notifications_type.rb
+++ app/graphql/types/notifications_type.rb
@@ -2,5 +2,9 @@ module Types
   class NotificationsType < Types::BaseObject
     field :pagination, Types::PaginationType, null: true
     field :data, [Types::NotificationType], null: true
+    field :notification_exist, Boolean, null: true
+    def notification_exist
+      object[:data].unread.present?
+    end
   end
 end
