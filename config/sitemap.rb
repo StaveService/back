c550db48bfe5423be862179b473720c80d3f569f
izszzz <izszzz@yahoo.co.jp>
2021-09-26 12:27:41 +0900

    rubocop

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://stave-back.com"
+SitemapGenerator::Sitemap.default_host = 'http://stave-back.com'
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
@@ -21,25 +21,25 @@ SitemapGenerator::Sitemap.create do
   #
   # Add all articles:
   #
-    add albums_path, :priority => 0.7, :changefreq => 'daily'
-    add artists_path, :priority => 0.7, :changefreq => 'daily'
-    add bands_path, :priority => 0.7, :changefreq => 'daily'
-    add musics_path, :priority => 0.7, :changefreq => 'daily'
-    add users_path, :priority => 0.7, :changefreq => 'daily'
+  add albums_path, priority: 0.7, changefreq: 'daily'
+  add artists_path, priority: 0.7, changefreq: 'daily'
+  add bands_path, priority: 0.7, changefreq: 'daily'
+  add musics_path, priority: 0.7, changefreq: 'daily'
+  add users_path, priority: 0.7, changefreq: 'daily'
 
-    Album.find_each do |album|
-      add album_path(album), :lastmod => album.updated_at
-    end
-    Artist.find_each do |artist|
-      add artist_path(artist), :lastmod => artist.updated_at
-    end
-    Band.find_each do |band|
-      add band_path(band), :lastmod => band.updated_at
-    end
-    Music.find_each do |music|
-      add music_path(music), :lastmod => music.updated_at
-    end
-    User.find_each do |user|
-      add user_path(user), :lastmod => user.updated_at
-    end
+  Album.find_each do |album|
+    add album_path(album), lastmod: album.updated_at
+  end
+  Artist.find_each do |artist|
+    add artist_path(artist), lastmod: artist.updated_at
+  end
+  Band.find_each do |band|
+    add band_path(band), lastmod: band.updated_at
+  end
+  Music.find_each do |music|
+    add music_path(music), lastmod: music.updated_at
+  end
+  User.find_each do |user|
+    add user_path(user), lastmod: user.updated_at
+  end
 end
